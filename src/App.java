2c0f5088b3aa6075432d34d3c48c2f5e243b7390
Wallison Carvalho <93956048+WallisonCarvalho@users.noreply.github.com>
2022-10-10 14:59:43 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite a rimeira nota: ");
+        System.out.print("Digite a primeira nota: ");
         double nota01 = sc.nextDouble();
         System.out.print("Digite a segunda nota: ");
         double nota02 = sc.nextDouble();
